b648faa4cca58cc3271346cd8d11127d7a4b5e85
George Ciachir <george.ciachirarhire@endava.com>
2017-11-26 09:49:42 +0200

    add spring profiles as string

--- src/main/java/george/projects/demos/configuration/SecurityProfile.java
+++ src/main/java/george/projects/demos/configuration/SecurityProfile.java
@@ -1,11 +1,9 @@
 package george.projects.demos.configuration;
 
 
-public class ApplicationSecurityProfile {
+public class SecurityProfile {
 
 	public static final String DEFAULT_AUTH_PROVIDER = "default_auth_provider";
 	public static final String CUSTOM_AUTH_PROVIDER = "custom_auth_provider";
-
-	public static final String ANOTHER = "another";
 
 }
