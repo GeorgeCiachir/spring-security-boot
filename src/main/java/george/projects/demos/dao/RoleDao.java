3be7f9df8ac37b6b403675fc71c921cf71bb3430
George Ciachir <george.ciachirarhire@endava.com>
2017-11-28 08:38:26 +0200

    extracted method from UserDao into RoleDao

--- src/main/java/george/projects/demos/dao/RoleDao.java
+++ src/main/java/george/projects/demos/dao/RoleDao.java
@@ -1,8 +1,32 @@
 package george.projects.demos.dao;
 
-/**
- * Created by gciachirarhire on 11/27/2017.
- */
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Service;
+
+import george.projects.demos.model.Role;
+
+@Service
 public class RoleDao {
 
+	private static final String ROLES_QUERY = "SELECT role_id, role_name FROM (SELECT * FROM user RIGHT JOIN user_has_role ON user.user_id = user_has_role.user_user_id where username = ?) as T INNER JOIN role on role_role_id = role_id";
+
+	private JdbcTemplate jdbcTemplate;
+
+	public Set<Role> findUserRoles(String username) {
+		return jdbcTemplate.query(ROLES_QUERY, new String[]{username}, (resultSet, rowNum) -> {
+			Role role = new Role();
+			role.setId(resultSet.getLong("role_id"));
+			role.setName(resultSet.getString("role_name"));
+			return role;
+		}).stream().collect(Collectors.toSet());
+	}
+
+	@Autowired
+	public void setJdbcTemplate(JdbcTemplate jdbcTemplate) {
+		this.jdbcTemplate = jdbcTemplate;
+	}
 }
--- src/main/java/george/projects/demos/dao/UserDao.java
+++ src/main/java/george/projects/demos/dao/UserDao.java
@@ -1,22 +1,16 @@
 package george.projects.demos.dao;
 
-import java.util.List;
-
-import javax.annotation.Resource;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 
-import george.projects.demos.model.Role;
 import george.projects.demos.model.User;
 
 @Service
 public class UserDao {
 
 	private static final String USERS_QUERY = "select * from user where username = ?";
-	private static final String ROLES_QUERY = "SELECT role_id, role_name FROM (SELECT * FROM user RIGHT JOIN user_has_role ON user.user_id = user_has_role.user_user_id where username = ?) as T INNER JOIN role on role_role_id = role_id";
 
-	@Resource
 	private JdbcTemplate jdbcTemplate;
 
 	public User findByUsername(String username) {
@@ -27,17 +21,12 @@ public class UserDao {
 			user.setEmail(resultSet.getString("email"));
 			user.setPassword(resultSet.getString("password"));
 			user.setEnabled(resultSet.getBoolean("enabled"));
-			user.setRoles(finUserRoles(username));
 			return user;
 		});
 	}
 
-	private List<Role> finUserRoles(String username) {
-		return jdbcTemplate.query(ROLES_QUERY, new String[]{username}, (resultSet, rowNum) -> {
-			Role role = new Role();
-			role.setId(resultSet.getLong("role_id"));
-			role.setName(resultSet.getString("role_name"));
-			return role;
-		});
+	@Autowired
+	public void setJdbcTemplate(JdbcTemplate jdbcTemplate) {
+		this.jdbcTemplate = jdbcTemplate;
 	}
 }
