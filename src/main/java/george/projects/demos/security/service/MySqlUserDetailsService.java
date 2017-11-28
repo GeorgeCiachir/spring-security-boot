aefca6c01b93d6353f4e028563ca3e189a74d101
George Ciachir <george.ciachirarhire@endava.com>
2017-11-28 17:56:24 +0200

    use RoleDao to retrieve user roles

--- src/main/java/george/projects/demos/security/service/MySqlUserDetailsService.java
+++ src/main/java/george/projects/demos/security/service/MySqlUserDetailsService.java
@@ -1,12 +1,11 @@
-package george.projects.demos.service;
+package george.projects.demos.security.service;
 
 import java.util.HashSet;
 import java.util.Set;
 
-import javax.annotation.Resource;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
@@ -15,17 +14,19 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import george.projects.demos.dao.RoleDao;
 import george.projects.demos.dao.UserDao;
 import george.projects.demos.model.Role;
 import george.projects.demos.model.User;
+import george.projects.demos.security.authentication.UserInfo;
 
 @Service
-public class DefaultUserDetailsService implements UserDetailsService {
+public class MySqlUserDetailsService implements UserDetailsService {
 
-	private static final Logger LOGGER = LoggerFactory.getLogger(DefaultUserDetailsService.class);
+	private static final Logger LOGGER = LoggerFactory.getLogger(MySqlUserDetailsService.class);
 
-	@Resource
 	private UserDao userDao;
+	private RoleDao roleDao;
 
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
@@ -33,15 +34,27 @@ public class DefaultUserDetailsService implements UserDetailsService {
 		try {
 			user = userDao.findByUsername(username);
 		} catch (EmptyResultDataAccessException e) {
-			LOGGER.info("nashpa rau");
+			LOGGER.info("Authentication failed; User not found");
 			throw new UsernameNotFoundException(username);
 		}
 
 		Set<GrantedAuthority> grantedAuthorities = new HashSet<>();
+		Set<Role> roles = roleDao.findUserRoles(username);
+		user.setRoles(roles);
 		for (Role role : user.getRoles()) {
 			grantedAuthorities.add(new SimpleGrantedAuthority(role.getName()));
 		}
 
-		return new org.springframework.security.core.userdetails.User(user.getUserName(), user.getPassword(), grantedAuthorities);
+		return new UserInfo(user.getUserName(), user.getPassword(), grantedAuthorities);
+	}
+
+	@Autowired
+	public void setUserDao(UserDao userDao) {
+		this.userDao = userDao;
+	}
+
+	@Autowired
+	public void setRoleDao(RoleDao roleDao) {
+		this.roleDao = roleDao;
 	}
 }
