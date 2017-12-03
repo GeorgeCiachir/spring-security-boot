4173d4e8b3a94fcd265d8b6c3ca88d52394d1936
George Ciachir <george.ciachirarhire@endava.com>
2017-12-03 06:24:19 +0200

    add additional maven profiles to run app in Tomcat and Jetty containers

---
+++ src/main/java/george/projects/demos/configuration/ApplicationSecurityProfile.java
@@ -0,0 +1,10 @@
+package george.projects.demos.configuration;
+
+
+public class SecurityProfile {
+
+	public static final String DEFAULT_AUTH_PROVIDER = "DEFAULT_AUTH_PROVIDER";
+	public static final String CUSTOM_AUTH_PROVIDER = "CUSTOM_AUTH_PROVIDER";
+	public static final String CUSTOM_SECURITY_FILTER = "CUSTOM_SECURITY_FILTER";
+
+}
