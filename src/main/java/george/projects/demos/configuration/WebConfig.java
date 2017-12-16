12d5d2bcdcce8bc038c9cfa92d3bb89293f8ad3d
George Ciachir <george.ciachirarhire@endava.com>
2017-12-16 10:35:05 +0200

    Add JSP dependencies and configure InternalResourceViewResolver

--- src/main/java/george/projects/demos/configuration/WebConfig.java
+++ src/main/java/george/projects/demos/configuration/WebConfig.java
@@ -1,51 +1,25 @@
-package com.demo.config;
+package george.projects.demos.configuration;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.Ordered;
-import org.springframework.web.context.request.RequestContextListener;
-import org.springframework.web.servlet.ViewResolver;
-import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
 
-/**
- * @author ankidaemon
- *
- */
+// class can be removed if the following properties are used in application.properties file
+// spring.mvc.view.prefix=/WEB-INF/jsp/
+// spring.mvc.view.suffix=.jsp
 @Configuration
 @EnableWebMvc
-@ComponentScan(basePackages = "com.demo")
 public class WebConfig extends WebMvcConfigurerAdapter {
-	@Bean
-	public ViewResolver viewResolver() {
-		InternalResourceViewResolver resolver = new InternalResourceViewResolver();
-		resolver.setPrefix("/WEB-INF/");
-		resolver.setSuffix(".jsp");
-		resolver.setExposeContextBeansAsAttributes(true);
-		resolver.setViewClass(JstlView.class);
-		return resolver;
-	}
-
-	@Bean
-	public RequestContextListener requestContextListener() {
-		return new RequestContextListener();
-	}
 
 	@Override
-	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
-		configurer.enable();
-	}
-	
-	/*
-	 * Section 2 - Video 2.4 - Added to customize login page*/
-	@Override
-	public void addViewControllers(ViewControllerRegistry registry) {
-		registry.addViewController("/login").setViewName("login");
-		registry.setOrder(Ordered.HIGHEST_PRECEDENCE);
+	public void configureViewResolvers(ViewResolverRegistry registry) {
+		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+		viewResolver.setPrefix("/WEB-INF/jsp/");
+		viewResolver.setSuffix(".jsp");
+		viewResolver.setViewClass(JstlView.class);
+		registry.viewResolver(viewResolver);
 	}
 }
