32623679395249b88114f1f5d793555ce9fef991
George Ciachir <george.ciachirarhire@endava.com>
2017-12-16 10:37:40 +0200

    Urls changes

--- src/main/java/george/projects/demos/controller/ApplicationController.java
+++ src/main/java/george/projects/demos/controller/ApplicationController.java
@@ -2,8 +2,8 @@ package george.projects.demos.controller;
 
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @RequestMapping(value = "/")
@@ -17,33 +17,27 @@ public class ApplicationController {
 	}
 
 	@ResponseBody
-	@RequestMapping(value = "/login/{name}")
-	public String login(@PathVariable(value = "name") String name) {
-		return "Hello " + name + " ! This is a clear route";
-	}
-
-	@ResponseBody
-	@RequestMapping(value = "/siteAdmin/{name}")
-	public String siteAdmin(@PathVariable(value = "name") String name) {
+	@RequestMapping(value = "/siteAdmin")
+	public String siteAdmin(@RequestParam(value = "name", required = false, defaultValue = "user") String name) {
 		return "Hello " + name + "! You are an authenticated site admin";
 	}
 
 	@ResponseBody
-	@RequestMapping(value = "/opsAdmin/{name}")
-	public String opsAdmin(@PathVariable(value = "name") String name) {
+	@RequestMapping(value = "/opsAdmin")
+	public String opsAdmin(@RequestParam(value = "name", required = false, defaultValue = "user") String name) {
 		return "Hello " + name + "! You are an authenticated ops admin";
 	}
 
 	@ResponseBody
-	@RequestMapping(value = "/normalUser/{name}")
-	public String normalUser(@PathVariable(value = "name") String name) {
+	@RequestMapping(value = "/normalUser")
+	public String normalUser(@RequestParam(value = "name", required = false, defaultValue = "user") String name) {
 		return "Hello " + name + " ! You are an authenticated normal user";
 	}
 
 	@PreAuthorize("hasRole('PREMIUM_USER')")
 	@ResponseBody
-	@RequestMapping(value = "/premiumUser/{name}")
-	public String premiumUser(@PathVariable(value = "name") String name) {
+	@RequestMapping(value = "/premiumUser")
+	public String premiumUser(@RequestParam(value = "name", required = false, defaultValue = "user") String name) {
 		return "Hello " + name + " ! You are an authenticated premium user";
 	}
 }
